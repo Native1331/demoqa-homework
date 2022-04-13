6fefc6b9a5b9efbf2288b19662896d911244bc55
Kadysheva Natalya <natalya.kadysheva@nedra.com>
2022-04-13 09:39:28 +0300

    Random Utils

---
+++ src/test/java/docs/StringFormatExamples.java
@@ -0,0 +1,9 @@
+/*import static java.lang.String.format;
+
+public static class StringFormatExample {
+
+    public static void main(String[] args) {
+        System.out.println(format("Hello,%s! How are %s", "username", "you"));
+    }
+}*/
+
